201226d91cf2c062dd697bd0d6fff54f66a91fe2
Sam Trenter <sntrenter@gmail.com>
2023-10-15 00:37:57 -0500

    got button thing conditional

--- TAOITEST/App.js
+++ TAOITEST/App.js
@@ -1,6 +1,6 @@
 import * as DocumentPicker from 'expo-document-picker';
 
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import { PermissionsAndroid, Platform } from 'react-native';
 
 import FileSystem from "expo-file-system"
@@ -29,7 +29,7 @@ async function importKML() {
   }
 }
 
-async function parseKMLFile(uri) {
+async function parseKMLFile_old(uri) {
   console.log('Parsing KML file:', uri);
   try {
     const response = await fetch(uri);
@@ -40,26 +40,47 @@ async function parseKMLFile(uri) {
         console.error('Error parsing KML:', error);
       } else {
         console.log('Parsed KML object:', result);
-        const shapes = {
-          shapes: []
-        };
-        console.log("TESTING:::",result.kml.Document[0].Folder[0].Placemark)
-        //check if there is a folder in the kml file
-        //if(result.kml.Document[0].Folder){
-          const placemarks = result.kml.Document[0].Folder[0].Placemark;
-        //}else{
-        //  const placemarks = result.kml.Document[0].Placemark;
-        //}
-        
-        placemarks.forEach(placemark => {
-          const name = placemark.name[0];
-          const coordinates = placemark.Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0];
-          const id = placemark.$.id; // Get the 'id' attribute
+        KMLJSON = JSON.stringify(result.Document.Folder.Placemark, null, 2);
+        KMLparsed = JSON.parse(KMLJSON);
+        const extractedData = [];
       
-          shapes.shapes.push({ name, coordinates, id });
-        });
-        console.log(shapes)
-        return shapes;
+        KMLparsed.forEach(item => {
+          const name = item.name._; // Get the 'name' property from the object
+          const id = item.id.value; // Get the 'id' property from the object
+          let points = [];
+      
+          // Check if the object contains 'Polygon' data
+          if (item.Polygon) {
+              const coordinates = item.Polygon.outerBoundaryIs.LinearRing.coordinates._;
+              // Split the coordinates into an array and convert to an array of objects, removing undefined or NaN entries
+              points = coordinates.split(' ')
+                  .map(coord => {
+                      const [longitude, latitude] = coord.split(',').map(parseFloat);
+                      if (!isNaN(longitude) && !isNaN(latitude)) {
+                          return { latitude, longitude };
+                      }
+                      return null;
+                  })
+                  .filter(point => point !== null);
+          }
+          // Check if the object contains 'Point' data
+          else if (item.Point) {
+              const coordinates = item.Point.coordinates._;
+              // Split the coordinates into an array and convert to an array of objects, removing undefined or NaN entries
+              points = coordinates.split(' ')
+                  .map(coord => {
+                      const [longitude, latitude] = coord.split(',').map(parseFloat);
+                      if (!isNaN(longitude) && !isNaN(latitude)) {
+                          return { latitude, longitude };
+                      }
+                      return null;
+                  })
+                  .filter(point => point !== null);
+          }
+      
+          extractedData.push({ name, id, points });
+      });
+      return extractedData;
       }
     });
   } catch (error) {
@@ -67,19 +88,107 @@ async function parseKMLFile(uri) {
   }
 }
 
+async function parseKMLFile(uri) {
+  try {
+    // Fetch the KML data from the specified URI
+    const response = await fetch(uri);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch KML data from ${uri}`);
+    }
+    const kmlData = await response.text();
+
+    // Create a custom parser with options to handle namespaces
+    const parser = new xml2js.Parser({
+      explicitArray: false,
+      mergeAttrs: true,
+      explicitRoot: false,
+      xmlns: true,
+    });
+
+    // Parse the XML
+    let result;
+    parser.parseString(kmlData, (err, parsedResult) => {
+      if (err) {
+        throw err;
+      }
+      result = parsedResult;
+    });
+
+    // Wait for parsing to complete
+    //require('deasync').loopWhile(() => !result);
+
+    if (!result.Document || !result.Document.Folder || !result.Document.Folder.Placemark) {
+      throw new Error('Invalid KML format');
+    }
+
+    const KMLJSON = JSON.stringify(result.Document.Folder.Placemark, null, 2);
+    const KMLparsed = JSON.parse(KMLJSON);
+    const extractedData = [];
+
+    KMLparsed.forEach(item => {
+      const name = item.name._;
+      const id = item.id.value;
+      let points = [];
+
+      if (item.Polygon) {
+        const coordinates = item.Polygon.outerBoundaryIs.LinearRing.coordinates._;
+        points = coordinates.split(' ')
+          .map(coord => {
+            const [longitude, latitude] = coord.split(',').map(parseFloat);
+            if (!isNaN(longitude) && !isNaN(latitude)) {
+              return { latitude, longitude };
+            }
+            return null;
+          })
+          .filter(point => point !== null);
+      } else if (item.Point) {
+        const coordinates = item.Point.coordinates._;
+        points = coordinates.split(' ')
+          .map(coord => {
+            const [longitude, latitude] = coord.split(',').map(parseFloat);
+            if (!isNaN(longitude) && !isNaN(latitude)) {
+              return { latitude, longitude };
+            }
+            return null;
+          })
+          .filter(point => point !== null);
+      }
+
+      extractedData.push({ name, id, points });
+    });
+    console.log("extractedData",extractedData)
+    return extractedData;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
 async function ingestKML(){
   let kmluri = await importKML();
-  let kmlJSON = await parseKMLFile(kmluri);
+  kmlJSON = await parseKMLFile(kmluri);
+  console.log(kmlJSON);
+}
+
+async function ExportJSON() {
+  console.log('Exporting JSON file.')
+}
+
+async function clearJSON(){
+  kmlJSON = {};
+  Alert.alert("KML JSON object cleared");
 }
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Button title="Import KML" onPress={() => ingestKML()} />
+      <Button color = {(Object.keys(kmlJSON).length === 0)?"grey":""} title="clear KML object" onPress={()=>clearJSON()} />
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hello World</Text>
       <Text>Test</Text>
+        <Button title="Export JSON" onPress={() => ExportJSON()} />
+        <Button title="check JSON" onPress={() => console.log("CHECK",kmlJSON)} /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -91,5 +200,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  disabledButton: {
+    opacity: 0.5,
+    backgroundColor: 'grey',
+  },
+  enabledButton: {
+    opacity: 1,
+    backgroundColor: 'green',
   },
 });
