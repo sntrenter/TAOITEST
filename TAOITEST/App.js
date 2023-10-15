8c99ce7b386d49cd860760cd6b48ab0249adbc3f
Sam Trenter <sntrenter@gmail.com>
2023-10-14 23:13:38 -0500

    opsie, think I added node_modules but doing some work to import kmls properly

--- TAOITEST/App.js
+++ TAOITEST/App.js
@@ -7,6 +7,28 @@ import FileSystem from "expo-file-system"
 import { StatusBar } from 'expo-status-bar';
 import xml2js from 'xml2js';
 
+let kmlJSON = {};
+//let shapes = {
+//  shapes: [
+//    {name: "", coordinates: [],id:""},
+//  ]
+//}
+
+async function importKML() {
+  console.log('Importing KML file.')
+  try {
+    const result = await DocumentPicker.getDocumentAsync();
+    console.log('DocumentPicker result:', result);
+    if (result.canceled === false && result.assets.length !== 0) {
+      return result.assets[0].uri;
+    } else {
+      console.log('Document picking canceled or failed.');
+    }
+  } catch (error) {
+    console.error('Error picking document:', error);
+  }
+}
+
 async function parseKMLFile(uri) {
   console.log('Parsing KML file:', uri);
   try {
@@ -18,7 +40,26 @@ async function parseKMLFile(uri) {
         console.error('Error parsing KML:', error);
       } else {
         console.log('Parsed KML object:', result);
-        // You can work with the parsed KML object here
+        const shapes = {
+          shapes: []
+        };
+        console.log("TESTING:::",result.kml.Document[0].Folder[0].Placemark)
+        //check if there is a folder in the kml file
+        //if(result.kml.Document[0].Folder){
+          const placemarks = result.kml.Document[0].Folder[0].Placemark;
+        //}else{
+        //  const placemarks = result.kml.Document[0].Placemark;
+        //}
+        
+        placemarks.forEach(placemark => {
+          const name = placemark.name[0];
+          const coordinates = placemark.Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0];
+          const id = placemark.$.id; // Get the 'id' attribute
+      
+          shapes.shapes.push({ name, coordinates, id });
+        });
+        console.log(shapes)
+        return shapes;
       }
     });
   } catch (error) {
@@ -27,28 +68,15 @@ async function parseKMLFile(uri) {
 }
 
 
-
-
-async function importKML() {
-  console.log('Importing KML file.')
-  try {
-    const result = await DocumentPicker.getDocumentAsync();
-    console.log('DocumentPicker result:', result);
-    if (result.canceled === false && result.assets.length !== 0) {
-      parseKMLFile(result.assets[0].uri);
-    } else {
-      console.log('Document picking canceled or failed.');
-    }
-  } catch (error) {
-    console.error('Error picking document:', error);
-  }
+async function ingestKML(){
+  let kmluri = await importKML();
+  let kmlJSON = await parseKMLFile(kmluri);
 }
-
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button title="Import KML" onPress={() => importKML()} />
+      <Button title="Import KML" onPress={() => ingestKML()} />
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hello World</Text>
       <Text>Test</Text>
---
+++ kmlreadertest.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+
+// Read the XML content from a file or variable
+const xml = fs.readFileSync('ATAK.one sqaure.kml', 'utf-8'); // Replace with your XML content
+
+const shapes = {
+  shapes: []
+};
+
+// Create a custom parser with options to handle namespaces
+const parser = new xml2js.Parser({
+  explicitArray: false,
+  mergeAttrs: true,
+  explicitRoot: false,
+  xmlns: true,
+});
+
+// Parse the XML
+parser.parseString(xml, (err, result) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  KMLJSON = JSON.stringify(result.Document.Folder.Placemark, null, 2);
+  KMLparsed = JSON.parse(KMLJSON);
+  const extractedData = [];
+
+  KMLparsed.forEach(item => {
+    const name = item.name._; // Get the 'name' property from the object
+    const id = item.id.value; // Get the 'id' property from the object
+    let points = [];
+
+    // Check if the object contains 'Polygon' data
+    if (item.Polygon) {
+      const coordinates = item.Polygon.outerBoundaryIs.LinearRing.coordinates._;
+      // Split the coordinates into an array and convert to points
+      points = coordinates.split(' ').map(coord => {
+        const [longitude, latitude] = coord.split(',').map(parseFloat);
+        return { longitude, latitude };
+      });
+    }
+    // Check if the object contains 'Point' data
+    else if (item.Point) {
+      const coordinates = item.Point.coordinates._;
+      // Split the coordinates into an array and convert to points
+      points = coordinates.split(' ').map(coord => {
+        const [longitude, latitude] = coord.split(',').map(parseFloat);
+        return { longitude, latitude };
+      });
+    }
+
+    extractedData.push({ name, id, points });
+  });
+  console.log(extractedData);
+});
